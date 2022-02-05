dbed371c5b97f93a5f417602efb8b5078f75cbd2
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-02-05 11:41:07 -0600

    feat: adding API documentation with swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,3 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty()
   name: string;
+  // * For optional fields, use @ApiPropertyOptional() or
+  // @ApiProperty({ required: false })
+  // age?: number;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,27 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @ApiOkResponse({ type: User, isArray: true })
   @Get()
   getUsers(): User[] {
     return this.usersService.findAll();
   }
 
+  @ApiOkResponse({ type: User, description: 'Return a user based on his url' })
   @Get(':id')
   getUser(@Param('id') id: string): User {
     return this.usersService.findOne(+id);
   }
 
+  @ApiCreatedResponse({ type: User, description: 'Creates a new user' })
   @Post()
   createUser(@Body() body: CreateUserDto): User {
     return this.usersService.createUser(body);
